5da1cf24ff1f9b4f3ca4aa80f6f51346b3fd5ed2
James E. Pustejovsky <jepusto@gmail.com>
2015-10-14 13:11:41 -0500

    Added naive z- and t-tests to coef_test. Added "All" option for test argument.

--- R/coef_test.R
+++ R/coef_test.R
@@ -51,8 +51,8 @@ saddlepoint <- function(t_stats, S_array) {
 
 #' Test all regression coefficients in a fitted model
 #' 
-#' \code{coef_test} reports t-tests for each coefficient estimate in a fitted
-#' linear regression model, using a sandwich estimator for the standard errors
+#' \code{coef_test} reports t-tests for each coefficient estimate in a fitted 
+#' linear regression model, using a sandwich estimator for the standard errors 
 #' and a small sample correction for the p-value. The small-sample correction is
 #' based on a Satterthwaite approximation or a saddlepoint approximation.
 #' 
@@ -61,9 +61,11 @@ saddlepoint <- function(t_stats, S_array) {
 #'   character string specifying which small-sample adjustment should be used to
 #'   calculate the variance-covariance.
 #' @param test Character vector specifying which small-sample corrections to 
-#'   calculate. \code{"Satterthwaite"} returns a Satterthwaite correction. 
-#'   \code{"saddlepoint"} returns a saddlepoint correction. Default is 
-#'   \code{"Satterthwaite"}.
+#'   calculate. \code{"z"} returns a z test (i.e., using a standard normal
+#'   reference distribution). \code{"naive-t"} returns a t test with \code{m -
+#'   1} degrees of freedom. \code{"Satterthwaite"} returns a Satterthwaite
+#'   correction. \code{"saddlepoint"} returns a saddlepoint correction. Default
+#'   is \code{"Satterthwaite"}.
 #' @param Ex_method Character string that controls how the expectation of the 
 #'   sandwich estimator is determined for use in the Satterthwaite 
 #'   approximation. The default, \code{"model"}, estimates the expectation based
@@ -89,19 +91,30 @@ coef_test <- function(obj, vcov, test = "Satterthwaite", Ex_method = "model", ..
   if (is.character(vcov)) vcov <- vcovCR(obj, type = vcov, ...)
   if (!("clubSandwich" %in% class(vcov))) stop("Variance-covariance matrix must be a clubSandwich.")
 
-  test <- match.arg(test, c("Satterthwaite","saddlepoint"), several.ok = TRUE)
+  all_tests <- c("z","naive-t","Satterthwaite","saddlepoint")
+  if (all(test == "All")) test <- all_tests
+  test <- match.arg(test, all_tests, several.ok = TRUE)
   Ex_method <- match.arg(Ex_method, c("model","empirical"), several.ok = TRUE)
   
   SE <- sqrt(diag(vcov))
   cluster <- attr(vcov, "cluster")
-  E_list <- attr(vcov, "estmats")
-  target <- attr(vcov, "target")
+  J <- nlevels(cluster)
   
-  S_array <- get_S_array(obj, cluster, target, E_list)
+  if (any(c("Satterthwaite","saddlepoint") %in% test)) {
+    E_list <- attr(vcov, "estmats")
+    target <- attr(vcov, "target")
+    S_array <- get_S_array(obj, cluster, target, E_list)
+  }
   
   result <- data.frame(beta = beta)
   result$SE[!beta_NA] <- SE
   
+  if ("z" %in% test) {
+    result$p_z[!beta_NA] <-  2 * pnorm(abs(beta[!beta_NA] / SE), lower.tail = FALSE)
+  }
+  if ("naive-t" %in% test) {
+    result$p_t[!beta_NA] <-  2 * pt(abs(beta[!beta_NA] / SE), df = J - 1, lower.tail = FALSE)
+  }
   if ("Satterthwaite" %in% test) {
     Satt <- Satterthwaite(beta = beta[!beta_NA], SE = SE, S_array = S_array, Ex_method = Ex_method)
     result$df[!beta_NA] <- Satt$df
@@ -126,10 +139,20 @@ coef_test <- function(obj, vcov, test = "Satterthwaite", Ex_method = "model", ..
 
 print.coef_test_clubSandwich <- function(x, digits = 3, ...) {
   res <- data.frame("Coef" = rownames(x), "Estimate" = x$beta, "Std. Error" = x$SE)
+  if ("p_z" %in% names(x)) {
+    p_z <- format.pval(x$p_z, digits = digits, eps = 10^-digits)
+    Sig_z <- cut(x$p_z, breaks = c(0, 0.001, 0.01, 0.05, 0.1, 1), 
+                 labels = c("***", "**", "*", ".", " "))
+    res <- cbind(res, "p-val (z)" = p_z, "Sig." = Sig_z)
+  }
+  if ("p_t" %in% names(x)) {
+    p_t <- format.pval(x$p_t, digits = digits, eps = 10^-digits)
+    Sig_t <- cut(x$p_t, breaks = c(0, 0.001, 0.01, 0.05, 0.1, 1), 
+                    labels = c("***", "**", "*", ".", " "))
+    res <- cbind(res, "p-val (naive-t)" = p_t, "Sig." = Sig_t)
+  }
   if ("df" %in% names(x)) {
     p_Satt <- format.pval(x$p_Satt, digits = digits, eps = 10^-digits)
-
- 
     Sig_Satt <- cut(x$p_Satt, breaks = c(0, 0.001, 0.01, 0.05, 0.1, 1), 
                        labels = c("***", "**", "*", ".", " "))
     res <- cbind(res, "d.f." = x$df, "p-val (Satt)" = p_Satt, "Sig." = Sig_Satt)    
