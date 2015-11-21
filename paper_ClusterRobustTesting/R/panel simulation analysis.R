a7d28b23cdd9e06df636004f57f73d2df70b456b
James E. Pustejovsky <jepusto@gmail.com>
2015-11-20 20:39:48 -0600

    Simulation results (for real!).

--- paper_ClusterRobustTesting/R/panel simulation analysis.R
+++ paper_ClusterRobustTesting/R/panel simulation analysis.R
@@ -10,38 +10,24 @@ results <- within(results, {
   constraints <- NULL
   seed <- NULL
   design <- names(design)
-  UB <- alpha + qnorm(0.95) * sqrt(alpha * (1 - alpha) / iterations)
 })
 head(results)
 
-results_long <- gather(results, "hypothesis", "reject", t_B:F_all)  
+gather(results, "alpha", "reject", alpha1:alpha10) %>%
+  mutate(alpha = as.numeric(substring(alpha, 6)) / 100) ->
+  results_long
 
 filter(results_long, test %in% c("CR1 Naive-F", "CR2 HTZ")) %>%
-  mutate(test = ifelse(test=="CR1 Naive-F", "Naive", "AHZ")) %>%
+  mutate(test = ifelse(test=="CR1 Naive-F", "Naive", "AHZ"),
+         UB = alpha + qnorm(0.95) * sqrt(alpha * (1 - alpha) / iterations)) %>%
+  select(-df) %>%
   spread(test, reject) ->
   results_compare
 
 
-# ICC = 0
+# alpha = .01
 
-filter(results_compare, alpha==0.05 & icc==0) %>%
-  group_by(design, m, n, hypothesis, alpha) %>%
-  summarise(AHZ = mean(AHZ), Naive = mean(Naive), iterations = sum(iterations)) %>%
-  mutate(UB = alpha + qnorm(0.95) * sqrt(alpha * (1 - alpha) / iterations)) %>%
-ggplot(aes(Naive, AHZ, color = factor(design))) + 
-  geom_point() + 
-  geom_hline(aes(yintercept = alpha)) + 
-  geom_vline(aes(xintercept = alpha)) + 
-  geom_hline(aes(yintercept = UB), linetype = "dashed") + 
-  geom_vline(aes(xintercept = UB), linetype = "dashed") + 
-  geom_abline(slope = 1) + 
-  facet_wrap(~ hypothesis, scales = "free") + 
-  labs(color = "design") + 
-  theme_minimal()
-
-# ICC > 0
-
-filter(results_compare, alpha==0.05 & icc > 0) %>%
+filter(results_compare, alpha==0.01) %>%
   ggplot(aes(Naive, AHZ, color = factor(design))) + 
   geom_point() + 
   geom_hline(aes(yintercept = alpha)) + 
@@ -49,7 +35,36 @@ filter(results_compare, alpha==0.05 & icc > 0) %>%
   geom_hline(aes(yintercept = UB), linetype = "dashed") + 
   geom_vline(aes(xintercept = UB), linetype = "dashed") + 
   geom_abline(slope = 1) + 
-  facet_grid(hypothesis ~ trt_var, scales = "free", labeller = "label_both") + 
+  facet_grid(m ~ hypothesis, scales = "free") + 
   labs(color = "design") + 
   theme_minimal()
 
+
+# alpha = .05
+
+filter(results_compare, alpha==0.05) %>%
+  ggplot(aes(Naive, AHZ, color = factor(design))) + 
+  geom_point() + 
+  geom_hline(aes(yintercept = alpha)) + 
+  geom_vline(aes(xintercept = alpha)) + 
+  geom_hline(aes(yintercept = UB), linetype = "dashed") + 
+  geom_vline(aes(xintercept = UB), linetype = "dashed") + 
+  geom_abline(slope = 1) + 
+  facet_grid(m ~ hypothesis, scales = "free") + 
+  labs(color = "design") + 
+  theme_minimal()
+
+
+# alpha = .10
+
+filter(results_compare, alpha==0.10) %>%
+  ggplot(aes(Naive, AHZ, color = factor(design))) + 
+  geom_point() + 
+  geom_hline(aes(yintercept = alpha)) + 
+  geom_vline(aes(xintercept = alpha)) + 
+  geom_hline(aes(yintercept = UB), linetype = "dashed") + 
+  geom_vline(aes(xintercept = UB), linetype = "dashed") + 
+  geom_abline(slope = 1) + 
+  facet_grid(m ~ hypothesis, scales = "free") + 
+  labs(color = "design") + 
+  theme_minimal()
