bef244c4397cfd8d871f2d237770f25cff698f57
James E. Pustejovsky <jepusto@gmail.com>
2016-01-05 15:53:37 -0600

    Need tests for lme objects.

---
+++ tests/testthat/test_lme.R
@@ -0,0 +1,11 @@
+context("lme objects")
+
+data("Orthodont", package = "nlme")
+fm1 <- lme(distance ~ age, random = ~ age, data = Orthodont)
+fm2 <- lme(distance ~ age + Sex, random = ~ 1, data = Orthodont)
+summary(fm1)
+summary(fm2)
+
+# test cluster specification
+# test target matrix specification
+# test inverse_var detection
