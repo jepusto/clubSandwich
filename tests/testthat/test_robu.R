834a83daf092e0463a5d0ea87ca372477e7eb84f
James E. Pustejovsky <jepusto@gmail.com>
2016-03-09 22:18:36 -0600

    Added tests for robu objects. Still working on debugging.

---
+++ tests/testthat/test_robu.R
@@ -0,0 +1,131 @@
+library(robumeta)
+context("robu objects: correlated effects")
+
+data(corrdat)
+
+test_that("CR0 z-tests agree with robumeta for correlated effects", {
+  corr_large <- robu(effectsize ~ males + college + binge, data = corrdat, 
+                         modelweights = "CORR", studynum = studyid,
+                         var.eff.size = var, small = FALSE)
+  p <- length(coef_CR(corr_large))
+  N <- corr_large$N
+  robu_CR0 <- vcovCR(corr_large, type = "CR0")
+  ztests <- coef_test(corr_large, vcov = robu_CR0 * N / (N - p), test = "z")
+  
+  expect_equivalent(corr_large$VR.r, as.matrix(robu_CR0))
+  expect_equivalent(corr_large$reg_table$SE, ztests$SE)
+  expect_equal(with(corr_large$reg_table, 2 * pnorm(abs(b.r / SE),lower.tail=FALSE)), 
+                    ztests$p_z)
+})
+
+test_that("CR2 t-tests agree with robumeta for correlated effects", {
+  corr_small <- robu(effectsize ~ males + college + binge, data = corrdat, 
+                         modelweights = "CORR", studynum = studyid,
+                         var.eff.size = var)
+  robu_CR2 <- vcovCR(corr_small, type = "CR2")
+  CR2_ttests <- coef_test(corr_small, vcov = robu_CR2, test = "Satterthwaite")
+    
+  expect_equivalent(corr_small$VR.r, as.matrix(robu_CR2))
+  expect_equal(corr_small$dfs, CR2_ttests$df)
+  expect_equal(corr_small$reg_table$prob, CR2_ttests$p_Satt)
+})
+
+
+test_that("CR0 z-tests agree with robumeta for hierarchical effects", {
+  hier_large <- robu(effectsize ~ binge + followup + sreport + age,
+                         data = hierdat, studynum = studyid,
+                         var.eff.size = var, modelweights = "HIER", small = FALSE)
+  p <- length(coef_CR(hier_large))
+  N <- hier_large$N
+  robu_CR0 <- vcovCR(hier_large, type = "CR0")
+  ztests <- coef_test(hier_large, vcov = robu_CR0 * N / (N - p), test = "z")
+  
+  expect_equivalent(hier_large$VR.r, as.matrix(robu_CR0))
+  expect_equivalent(hier_large$reg_table$SE, ztests$SE)
+  expect_equal(with(hier_large$reg_table, 2 * pnorm(abs(b.r / SE),lower.tail=FALSE)), 
+               ztests$p_z)
+})
+
+test_that("CR2 t-tests agree with robumeta for hierarchical effects", {
+  corr_small <- robu(effectsize ~ binge + followup + sreport + age,
+                         data = hierdat, studynum = studyid,
+                         var.eff.size = var, modelweights = "HIER")
+  robu_CR2 <- vcovCR(corr_small, type = "CR2")
+  CR2_ttests <- coef_test(corr_small, vcov = robu_CR2, test = "Satterthwaite")
+  
+  expect_equivalent(corr_small$VR.r, as.matrix(robu_CR2))
+  expect_equal(corr_small$dfs, CR2_ttests$df)
+  expect_equal(corr_small$reg_table$prob, CR2_ttests$p_Satt)
+})
+
+test_that("CR0 z-tests agree with robumeta for hierarchical effects", {
+  hier_large <- robu(effectsize ~ binge + followup + sreport + age,
+                     data = hierdat, studynum = studyid,
+                     var.eff.size = var, modelweights = "HIER", small = FALSE)
+  p <- length(coef_CR(hier_large))
+  N <- hier_large$N
+  robu_CR0 <- vcovCR(hier_large, type = "CR0")
+  ztests <- coef_test(hier_large, vcov = robu_CR0 * N / (N - p), test = "z")
+  
+  expect_equivalent(hier_large$VR.r, as.matrix(robu_CR0))
+  expect_equivalent(hier_large$reg_table$SE, ztests$SE)
+  expect_equal(with(hier_large$reg_table, 2 * pnorm(abs(b.r / SE),lower.tail=FALSE)), 
+               ztests$p_z)
+})
+
+data(dropoutPrevention)
+
+test_that("dropoutPrevention tests replicate Tipton & Pustejovsky (2015) - full sample", {
+  m3_hier <- robu(LOR1 ~ study_design + attrition + group_equivalence + adjusted
+                  + outcome + evaluator_independence
+                  + male_pct + white_pct + average_age
+                  + implementation_quality + program_site + duration + service_hrs, 
+                  data = dropoutPrevention, studynum = studyID, var.eff.size = varLOR, modelweights = "HIER")
+  
+  contrast_list <- list("Study design" = 2:3, 
+                        "Outcome measure" = 7:9,
+                        "Evaluator independence" = 10:12,
+                        "Implmentation quality" = 16:17,
+                        "Program format" = 18:20)
+  
+  dropout_tests <- Wald_test(m3_hier, constraints = contrast_list, 
+                             vcov = "CR2", test = c("Naive-F","HTZ"))
+  
+  Fstat_club <- sapply(dropout_tests, function(x) x$F)
+  Fstat_paper <- matrix(c(0.23, 0.22, 0.91, 0.84, 3.11, 2.78, 14.15, 13.78, 3.85, 3.65), nrow = 2)
+  expect_equivalent(Fstat_paper, round(Fstat_club, 2))
+  
+  df_club <- sapply(dropout_tests, function(x) x$df[2])
+  df_paper <- c(42.9, 21.5, 16.8, 36.9, 37.5)
+  expect_equivalent(df_paper, round(df_club, 1))
+})
+
+test_that("dropoutPrevention tests replicate Tipton & Pustejovsky (2015) - reduced sample", {
+  m3_hier <- robu(LOR1 ~ study_design + attrition + group_equivalence + adjusted
+                  + outcome + evaluator_independence
+                  + male_pct + white_pct + average_age
+                  + implementation_quality + program_site + duration + service_hrs, 
+                  data = subset(dropoutPrevention, big_study==TRUE),
+                  studynum = studyID, var.eff.size = varLOR, modelweights = "HIER")
+  
+  contrast_list <- list("Study design" = 2:3, 
+                        "Outcome measure" = 7:9,
+                        "Evaluator independence" = 10:11,
+                        "Implmentation quality" = 15:16,
+                        "Program format" = 17:19)
+  
+  dropout_tests <- Wald_test(m3_hier, constraints = contrast_list, 
+                             vcov = "CR2", test = c("Naive-F","HTZ"))
+  
+  Fstat_club <- sapply(dropout_tests, function(x) x$F)
+  Fstat_paper <- matrix(c(3.19, 2.93, 1.05, 0.84, 0.32, 0.26, 4.02, 3.69, 1.19, 0.98), nrow = 2)
+  expect_equivalent(Fstat_paper, round(Fstat_club, 2))
+  
+  df_club <- sapply(dropout_tests, function(x) x$df[2])
+  df_paper <- c(11.0, 7.7, 4.6, 11.0, 9.1)
+  expect_equivalent(df_paper, round(df_club, 1))
+})
+
+# test cluster specification
+# test target matrix specification
+# test inverse_var detection
