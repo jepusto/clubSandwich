2596b7b81ab2ef9a52cadaa251ad389982f2afc4
James E. Pustejovsky <jepusto@gmail.com>
2016-04-05 12:39:23 -0500

    Unit tests for plm (fixed and random effects).

--- tests/testthat/test_plm-fixed-effects.R
+++ tests/testthat/test_plm-fixed-effects.R
@@ -1,4 +1,4 @@
-context("plm objects")
+context("plm objects - fixed effects")
 
 library(plm)
 
@@ -6,10 +6,11 @@ data("Produc", package = "plm")
 Produc$cluster <- sample(LETTERS[1:10], size = nrow(Produc), replace=TRUE)
 Produc_scramble <- Produc[sample(nrow(Produc)),]
 
+plm_individual <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, 
+                      data = Produc_scramble, index = c("state","year"), 
+                      effect = "individual", model = "within")
+
 test_that("individual effects agree with lm", {
-  plm_individual <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, 
-                             data = Produc_scramble, index = c("state","year"), 
-                             effect = "individual", model = "within")
   lm_individual <- lm(log(gsp) ~ 0 + state + log(pcap) + log(pc) + log(emp) + unemp, data = Produc)
   coef_names <- names(coef(plm_individual)) 
   coef_index <- names(coef(lm_individual)) %in% coef_names
@@ -22,10 +23,12 @@ test_that("individual effects agree with lm", {
                vcovCR(lm_individual, cluster = Produc$state, type = "CR2")[coef_index,coef_index])
 })
 
+plm_time <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, 
+                data = Produc_scramble, index = c("state","year"), 
+                effect = "time", model = "within")
+
 test_that("time effects agree with lm", {
-  plm_time <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, 
-                       data = Produc_scramble, index = c("state","year"), 
-                       effect = "time", model = "within")
+  
   lm_time <- lm(log(gsp) ~ 0 + factor(year) + log(pcap) + log(pc) + log(emp) + unemp, data = Produc)
   coef_names <- names(coef(plm_time)) 
   coef_index <- names(coef(lm_time)) %in% coef_names
@@ -38,10 +41,12 @@ test_that("time effects agree with lm", {
                vcovCR(lm_time, cluster = Produc$year, type = "CR2")[coef_index,coef_index])
 })
 
+plm_twoways <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, 
+                   data = Produc_scramble, index = c("state","year"), 
+                   effect = "twoways", model = "within")
+
 test_that("two-way effects agree with lm", {
-  plm_twoways <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, 
-                          data = Produc_scramble, index = c("state","year"), 
-                          effect = "twoways", model = "within")
+  
   lm_twoways <- lm(log(gsp) ~ 0 + state + factor(year) + log(pcap) + log(pc) + log(emp) + unemp, data = Produc)
   coef_names <- names(coef(plm_twoways)) 
   coef_index <- names(coef(lm_twoways)) %in% coef_names
@@ -72,7 +77,71 @@ test_that("two-way effects agree with lm", {
   
 })
 
-# test for equality with HC when cluster = rownames(data)
-# test cluster specification
-# test target matrix specification
-# test inverse_var detection
+plm_fit <- plm_individual
+n <- nrow(Produc_scramble)
+
+obj <- plm_fit
+type <- "CR2"
+inverse_var <- FALSE
+target <- NULL
+index <- attr(model.frame(obj),"index")
+cluster <- switch(obj$args$effect,
+                  individual = index[[1]],
+                  time = index[[2]])
+
+test_that("vcovCR options work for CR2", {
+  CR2_iv <- vcovCR(plm_fit, type = "CR2")
+  expect_identical(vcovCR(plm_fit, cluster = Produc_scramble$state, type = "CR2"), CR2_iv)
+  expect_identical(vcovCR(plm_fit, type = "CR2", inverse_var = TRUE), CR2_iv)
+  expect_identical(vcovCR(plm_fit, type = "CR2", target = rep(1, n), inverse_var = TRUE), CR2_iv)
+  
+  CR2_not <- vcovCR(plm_fit, type = "CR2", inverse_var = FALSE)
+  expect_equivalent(CR2_not, CR2_iv)
+  expect_identical(vcovCR(plm_fit, cluster = Produc_scramble$state, type = "CR2", inverse_var = FALSE), CR2_not)
+  expect_identical(vcovCR(plm_fit, type = "CR2", target = rep(1, n)), CR2_not)
+  expect_identical(vcovCR(plm_fit, type = "CR2", target = rep(1, n), inverse_var = FALSE), CR2_not)
+  expect_false(identical(vcovCR(plm_fit, type = "CR2", target = 1 / Produc_scramble$emp), CR2_not))
+})
+
+test_that("vcovCR options work for CR4", {
+  CR4_iv <- vcovCR(plm_fit, type = "CR4")
+  expect_identical(vcovCR(plm_fit, cluster = Produc_scramble$state, type = "CR4"), CR4_iv)
+  expect_identical(vcovCR(plm_fit, type = "CR4", inverse_var = TRUE), CR4_iv)
+  expect_identical(vcovCR(plm_fit, type = "CR4", target = rep(1, n), inverse_var = TRUE), CR4_iv)
+  
+  CR4_not <- vcovCR(plm_fit, type = "CR4", inverse_var = FALSE)
+  expect_equivalent(CR4_not, CR4_iv)
+  expect_identical(vcovCR(plm_fit, cluster = Produc_scramble$state, type = "CR4", inverse_var = FALSE), CR4_not)
+  expect_identical(vcovCR(plm_fit, type = "CR4", target = rep(1, n)), CR4_not)
+  expect_identical(vcovCR(plm_fit, type = "CR4", target = rep(1, n), inverse_var = FALSE), CR4_not)
+  expect_false(identical(vcovCR(plm_fit, type = "CR4", target = 1 / Produc_scramble$emp), CR4_not))
+  
+})
+
+test_that("CR2 and CR4 are target-unbiased", {
+  
+  expect_true(check_CR(plm_individual, vcov = "CR2"))
+  expect_true(check_CR(plm_individual, vcov = "CR4"))
+  
+  expect_true(check_CR(plm_time, vcov = "CR2"))
+  expect_true(check_CR(plm_time, vcov = "CR4"))
+  
+  expect_true(check_CR(plm_twoways, vcov = "CR2", cluster = "individual"))
+  expect_true(check_CR(plm_twoways, vcov = "CR4", cluster = "individual"))
+  expect_true(check_CR(plm_twoways, vcov = "CR2", cluster = "time"))
+  expect_true(check_CR(plm_twoways, vcov = "CR4", cluster = "time"))
+  expect_true(check_CR(plm_twoways, vcov = "CR2", cluster = Produc_scramble$cluster))
+  expect_true(check_CR(plm_twoways, vcov = "CR4", cluster = Produc_scramble$cluster))
+})
+
+test_that("vcovCR is equivalent to vcovHC when clusters are all of size 1", {
+  library(sandwich)
+  CR0 <- vcovCR(plm_fit, cluster = 1:n, type = "CR0")
+  expect_equal(vcovHC(plm_fit, type = "HC0"), as.matrix(CR0))
+  CR1 <- vcovCR(plm_fit, cluster = 1:n, type = "CR1S")
+  expect_equal(vcovHC(plm_fit, type = "HC1"), as.matrix(CR1) * (n - 1) / n)
+  CR2 <- vcovCR(plm_fit, cluster = 1:n, type = "CR2")
+  expect_equal(vcovHC(plm_fit, type = "HC2"), as.matrix(CR2))
+  CR3 <- vcovCR(plm_fit, cluster = 1:n, type = "CR3")
+  expect_equal(vcovHC(plm_fit, type = "HC3"), as.matrix(CR3))
+})
--- tests/testthat/test_plm-random-effects.R
+++ tests/testthat/test_plm-random-effects.R
@@ -1,4 +1,4 @@
-context("plm objects")
+context("plm objects - random effects")
 
 library(nlme)
 library(plm)
@@ -14,20 +14,25 @@ CR_types <- paste0("CR",0:4)
 
 test_that("individual effects agree with gls", {
   plm_individual <- plm(inv ~ value + capital, data = Grunfeld, model="random")
-
+  obj <- plm_individual
   icc <- with(plm_individual$ercomp$sigma2, id / (id + idios))
   gls_individual <- gls(inv ~ value + capital, data = Grunfeld,
-                        correlation = corCompSymm(value = icc, form = ~ 1 | firm))
+                        correlation = corCompSymm(value = icc, form = ~ 1 | firm, fixed=TRUE))
+  
+  expect_equal(model_matrix(plm_individual), model_matrix(gls_individual))
+  expect_identical(nobs(plm_individual), nobs(gls_individual))
+  V_ratio <- targetVariance(plm_individual) / targetVariance(gls_individual)
+  V_ratio <- V_ratio[is.finite(V_ratio)]
+  expect_equal(min(V_ratio), max(V_ratio))
+  expect_equivalent(residuals_CR(plm_individual), residuals_CR(gls_individual))
 
   CR_plm <- lapply(CR_types, function(x) vcovCR(plm_individual, type = x))
   CR_gls <- lapply(CR_types, function(x) vcovCR(gls_individual, type = x))
-  expect_less_than(max(mapply(function(x, y) max(abs(range(x / y) - 1)), 
-                              x = CR_plm, y = CR_gls)), .06)
+  expect_equivalent(CR_plm, CR_gls)
 
   test_plm <- lapply(CR_types, function(x) coef_test(plm_individual, vcov = x, test = "All")[,-3])
   test_gls <- lapply(CR_types, function(x) coef_test(gls_individual, vcov = x, test = "All")[,-3])
-  expect_less_than(max(mapply(function(x, y) max(abs(range(x / y) - 1)), 
-                              x = test_plm, y = test_gls)), .10)
+  expect_equivalent(test_plm, test_gls)
   
 })
 
@@ -35,20 +40,27 @@ test_that("time effects agree with gls", {
   plm_time <- plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, 
                   data = Produc, index = c("state","year"), 
                   effect = "time", model = "random")
+  obj <- plm_time
   icc <- with(plm_time$ercomp$sigma2, id / (id + idios))
   gls_time <- gls(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, 
                   data = Produc, 
-                  correlation = corCompSymm(value = icc, form = ~ 1 | year))
+                  correlation = corCompSymm(value = icc, form = ~ 1 | year, fixed = TRUE))
+  
+  expect_equal(model_matrix(plm_time), model_matrix(gls_time))
+  expect_identical(nobs(plm_time), nobs(gls_time))
+  V_ratio <- targetVariance(plm_time) / targetVariance(gls_time)
+  V_ratio <- V_ratio[is.finite(V_ratio)]
+  expect_equal(min(V_ratio), max(V_ratio))
+  expect_equivalent(residuals_CR(plm_time), residuals_CR(gls_time))
   
   CR_plm <- lapply(CR_types, function(x) vcovCR(plm_time, type = x))
   CR_gls <- lapply(CR_types, function(x) vcovCR(gls_time, type = x))
-  expect_less_than(max(mapply(function(x, y) max(abs(range(x / y) - 1)), 
-                              x = CR_plm, y = CR_gls)), .06)
+  expect_equivalent(CR_plm, CR_gls)
   
   test_plm <- lapply(CR_types, function(x) coef_test(plm_time, vcov = x, test = "All")[,-3])
   test_gls <- lapply(CR_types, function(x) coef_test(gls_time, vcov = x, test = "All")[,-3])
-  expect_less_than(max(mapply(function(x, y) max(abs(range(x / y) - 1)), 
-                              x = test_plm, y = test_gls)), .10)
+  expect_equivalent(test_plm, test_gls)
+  
 })
 
 test_that("two-way effects throws error", {
