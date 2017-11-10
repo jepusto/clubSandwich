1e9c6f6ec043c172d578ddc1fe8597d78c00cb0b
jepusto <jepusto@gmail.com>
2017-11-10 16:06:56 -0600

    Fixed residuals_CS.plm.

--- R/plm.R
+++ R/plm.R
@@ -176,6 +176,9 @@ augmented_model_matrix.plm <- function(obj, cluster, inverse_var, ignore_FE) {
 residuals_CS.plm <- function(obj) {
   if (obj$args$model=="random") {
     y <- plm::pmodel.response(formula(obj), model.frame(obj), model = "pooling")
+    nm <- names(y)
+    y <- as.numeric(y)
+    names(y) <- nm
     Xb <- as.numeric(model_matrix(obj) %*% coef(obj))
     y - Xb
   } else {
