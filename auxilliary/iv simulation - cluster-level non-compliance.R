1f25b00e1908009ae6744ec441fecce587c8ffca
jepusto <jepusto@gmail.com>
2017-11-21 16:16:57 -0600

    Reparameterized cluster IV simulation.

--- auxilliary/iv simulation - cluster-level non-compliance.R
+++ auxilliary/iv simulation - cluster-level non-compliance.R
@@ -4,38 +4,41 @@ rm(list = ls())
 # Data Generating Model
 #------------------------------------------------------
 
-# clusters <- 20
-# trt_clusters <- 14
+# clusters <- 40
+# p_trt <- 0.5
 # delta <- 0
 # size_mean <- 5
-# c1 <- qnorm(0.1)
-# c2 <- qnorm(0.8)
-# c3 <- 0.7
+# p_nt <- 0.1
+# p_at <- 0.1
+# v_uc <- 10
 # r_uy <- 0.8
-# icc <- 0.3
+# icc <- 0.2
 
-r_cluster_trial <- function(clusters, trt_clusters, 
+r_cluster_trial <- function(clusters, p_trt = 0.5, 
                             delta = 0, size_mean = 10, 
-                            c1 = qnorm(0.1), c2 = qnorm(0.9), c3 = 0.7, 
-                            r_uy = 0.8, icc = 0.3) {
+                            p_nt = 0.1, p_at = p_nt, v_uc = 10, 
+                            r_uy = 0.8, icc = 0.2) {
   
   n_j <- 1 + rpois(clusters, lambda = size_mean)
   cluster_id <- rep(1:clusters, n_j)
-  Trt <- (1:clusters) %in% (1:trt_clusters)
+  Trt <- (1:clusters) %in% (1:round(p_trt * clusters))
   
-  U <- rnorm(clusters, mean = 0, sd = sqrt(icc))
-  D0 <- rbinom(clusters, size = 1, prob = pnorm(c1 + c3 * U))
-  D1 <- rbinom(clusters, size = 1, prob = pnorm(c2 + c3 * U))
-  D1[D1 < D0] <- D0[D1 < D0]
+  U <- runif(clusters)
+  D0 <- rbinom(clusters, size = 1, prob = qbeta(U, shape1 = v_uc * p_at, shape2 = v_uc * (1 - p_at)))
+  D1 <- rbinom(clusters, size = 1, prob = qbeta(U, shape1 = v_uc * (1 - p_nt) / (1 - p_at), shape2 = v_uc * p_nt / (1 - p_at)))
+  D1 <- ifelse(D0 == 1, 1L, D1)
   D <- ((1 - Trt) * D0 + Trt * D1)[cluster_id]
+  table(D0, D1)
   
-  Y0 <- rnorm(clusters, mean = r_uy * U, sd = sqrt((1 - r_uy^2) * icc))[cluster_id] + rnorm(sum(n_j), mean = 0, sd = sqrt(1 - icc))
+  Y0 <- r_uy * qnorm(U, mean = 0, sd = sqrt(icc))[cluster_id] + 
+          rnorm(clusters, mean = 0, sd = sqrt((1 - r_uy^2) * icc))[cluster_id] + 
+            rnorm(sum(n_j), mean = 0, sd = sqrt(1 - icc))
   Y <- Y0 + delta * D  
   
   data.frame(cluster = cluster_id, Trt = as.integer(Trt[cluster_id]), D = D, Y = Y)
 }
 
-# dat <- r_cluster_trial(clusters, trt_clusters, delta, size_mean, c1, c2, c3, r_uy, icc)
+# dat <- r_cluster_trial(clusters, p_trt, delta, size_mean, p_nt, p_at, v_uc, r_uy, icc)
 
 #------------------------------------------------------
 # Model-fitting function
@@ -45,17 +48,16 @@ iv_est <- function(dat) {
   require(AER)
   require(clubSandwich)
   lm_fit <- lm(Y ~ D, data = dat)
-  lm_CR2 <- coef_test(lm_fit, cluster = dat$cluster, vcov = "CR2", test = "Satterthwaite")["D",c("beta","SE","p_Satt"),drop=FALSE]
+  lm_CR2 <- coef_test(lm_fit, cluster = dat$cluster, vcov = "CR2", test = c("z", "Satterthwaite"))["D",,drop=FALSE]
   rownames(lm_CR2) <- NULL
-  names(lm_CR2)[3] <- "pval"
-
+  
   iv_fit <- ivreg(Y ~ D | Trt, data = dat)
-  iv_CR0 <- coef_test(iv_fit, cluster = dat$cluster, vcov = "CR0", test = "z")["D",,drop=FALSE]
+  iv_CR0 <- coef_test(iv_fit, cluster = dat$cluster, vcov = "CR0", test = c("z", "Satterthwaite"))["D",,drop=FALSE]
   rownames(iv_CR0) <- NULL
-  names(iv_CR0)[3] <- "pval"
-  iv_CR2 <- coef_test(iv_fit, cluster = dat$cluster, vcov = "CR2", test = "Satterthwaite")["D",c("beta","SE","p_Satt"),drop=FALSE]
+  
+  iv_CR2 <- coef_test(iv_fit, cluster = dat$cluster, vcov = "CR2", test = c("z", "Satterthwaite"))["D",,drop=FALSE]
   rownames(iv_CR2) <- NULL
-  names(iv_CR2)[3] <- "pval"
+  
   data.frame(type = c("lm-CR2","iv-CR0","iv-CR2"), rbind(lm_CR2, iv_CR0, iv_CR2))
 }
 
@@ -78,16 +80,16 @@ check_pvals(x = runif(10000), alpha = c(0.01, 0.05, 0.10))
 # Simulation Driver
 #-----------------------------------------------------------
 
-simulate_IV <- function(replicates, clusters, trt_clusters = clusters / 2, size_mean = 5, 
-                        c1 = qnorm(0.1), c2 = -c1, c3 = 0.7, r_uy = 0.8, icc = 0.3,
+simulate_IV <- function(replicates, clusters, p_trt = 0.5, size_mean = 5, 
+                        p_nt = 0.1, p_at = p_nt, v_uc = 10, r_uy = 0.8, icc = 0.2,
                         alpha = c(.01, .05), seed = NULL) {
   require(dplyr) 
   require(tidyr)
   if (!is.null(seed)) set.seed(seed)
   
   reps <- replicate(replicates, {
-    dat <- r_cluster_trial(clusters = clusters, trt_clusters = trt_clusters, 
-                           size_mean = size_mean, c1 = c1, c2 = c2, c3 = c3,
+    dat <- r_cluster_trial(clusters = clusters, p_trt = p_trt, size_mean = size_mean, 
+                           p_nt = p_nt, p_at = p_at, v_uc = v_uc,
                            r_uy = r_uy, icc = icc)
     iv_est(dat)
   }, simplify = FALSE)
@@ -100,13 +102,15 @@ simulate_IV <- function(replicates, clusters, trt_clusters = clusters / 2, size_
       V_beta = var(beta),
       E_var = mean(V),
       V_var = var(V),
-      reject = check_pvals(pval, alpha = alpha)
+      z = check_pvals(p_z, alpha = alpha),
+      Satt = check_pvals(p_Satt, alpha = alpha)
     ) %>%
+    gather("test","reject", z, Satt) %>%
     unnest(reject)
 }
 
-# simulate_IV(replicates = 100, clusters = 20, trt_clusters = 10, size_mean = 5,
-#             c1 = qnorm(0.1), c2 = qnorm(0.9), c3 = 0.7,
+# simulate_IV(replicates = 100, clusters = 20, p_trt = 0.4, size_mean = 5,
+#             p_nt = 0.2, p_at = 0.2, v_uc = 5,
 #             r_uy = 0.8, icc = 0.3)
 
 
@@ -115,12 +119,24 @@ simulate_IV <- function(replicates, clusters, trt_clusters = clusters / 2, size_
 #-------------------------------------
 source_obj <- ls()
 
-set.seed(20171119)
+set.seed(20171120)
 
-design_factors <- list(clusters = seq(20, 60, 10), c1 = qnorm(c(0.1, 0.2)), 
-                       icc = c(0.1, 0.2, 0.3))
+
+# clusters <- 40
+# p_trt <- 0.5
+# delta <- 0
+# size_mean <- 5
+# p_nt <- 0.1
+# p_at <- 0.1
+# v_uc <- 10
+# r_uy <- 0.8
+# icc <- 0.3
+
+design_factors <- list(clusters = seq(20, 60, 10), p_trt = c(.3, .5), 
+                       icc = 0.2, v_uc = c(1, 2, 5, 10),
+                       p_nt = seq(0, 0.25, 0.05))
 params <- expand.grid(design_factors)
-params$replicates <- 50
+params$replicates <- 5000
 params$seed <- round(runif(1) * 2^30) + 1:nrow(params)
 
 # All look right?
@@ -156,12 +172,13 @@ library(ggplot2)
 rm(list=ls())
 load("auxilliary/IV Simulation Results.Rdata")
 
-results$compliance <- 1 - 2 * pnorm(results$c1)
+results$compliance <- with(results, 1 - 2 * p_nt)
 
-ggplot(results, aes(clusters, alpha_0.05, color = type)) + 
+ggplot(results, aes(compliance, alpha_0.05, color = type, shape = type, linetype = test)) + 
   geom_line() + geom_point() + 
   geom_hline(yintercept = .05, linetype = "dashed") + 
-  facet_grid(compliance ~ icc, labeller = "label_both") + 
+  facet_grid(p_trt + v_uc ~ clusters, labeller = "label_both") + 
+  coord_cartesian(ylim = c(0, 0.1)) + 
   theme_light() + 
   labs(x = "Number of clusters", y = "Rejection rate at alpha = .05") + 
   theme(legend.position = "bottom")
