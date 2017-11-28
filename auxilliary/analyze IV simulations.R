a8b4466e64374c5c9296bf622a97e082c857d0b9
James E. Pustejovsky <jepusto@gmail.com>
2017-11-27 22:38:42 -0600

    setting up iv simulations for TACC.

---
+++ auxilliary/analyze IV simulations.R
@@ -0,0 +1,6 @@
+#--------------------------------------------------------
+# Analyze simulation results
+#--------------------------------------------------------
+library(ggplot2)
+rm(list=ls())
+load("auxilliary/IV Multisite Simulation Results.Rdata")
