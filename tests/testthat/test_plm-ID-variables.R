3e296cb745af41ec8e54552b8a46f994570bd52c
jepusto <jepusto@gmail.com>
2019-05-08 22:28:21 -0500

    Added tests for plm objects with various methods of specifying panel indices.

--- tests/testthat/test_plm-ID-variables.R
+++ tests/testthat/test_plm-ID-variables.R
@@ -86,3 +86,48 @@ test_that("plm works for Yuki Takahashi's reprex.",{
   expect_s3_class(vcovCR(fe_fit, cluster=Data$gid, type = "CR2"), "vcovCR") 
   
 })
+
+
+
+test_that("Clustering works for various ways of specifying unit and time indices in plm.", {
+  
+  data("Grunfeld", package = "plm")
+  Grunfeld$cluster <- sample(LETTERS[1:10], size = nrow(Grunfeld), replace=TRUE)
+  rearrange <- mapply(function(s,b) seq(s, nrow(Grunfeld), b), 1:10, 11:20)
+  rearrange <- unique(unlist(rearrange))
+  rearrange <- c(rearrange, setdiff(1:200, rearrange))
+  Grunfeld_scramble <- Grunfeld[rearrange,]
+  Grunfeld_pdata <- pdata.frame(Grunfeld_scramble, index = c("firm","year"))
+  
+  plm_pdata <- plm(inv ~ value + capital, data = Grunfeld_pdata, model="within")
+  plm_numeric <- plm(inv ~ value + capital, data = Grunfeld, index = 10, model="within")
+  plm_noindex <- plm(inv ~ value + capital, data = Grunfeld_scramble, model="within")
+  plm_oneindex <- plm(inv ~ value + capital, data = Grunfeld_scramble, index = "firm", model="within")
+  plm_twoindex <- plm(inv ~ value + capital, data = Grunfeld_scramble, index = c("firm","year"), model="within")
+
+  CR_types <- paste0("CR",0:3)
+  
+  # auto clustering
+  vcovCRs <- function(model, types) lapply(types, function(x) vcovCR(model, type = x))
+  CR_pdata <- vcovCRs(plm_pdata, CR_types)
+  expect_equivalent(CR_pdata, vcovCRs(plm_numeric, CR_types))
+  expect_equivalent(CR_pdata, vcovCRs(plm_noindex, CR_types))
+  expect_equivalent(CR_pdata, vcovCRs(plm_oneindex, CR_types))
+  expect_equivalent(CR_pdata, vcovCRs(plm_twoindex, CR_types))
+  
+  # manual clustering on firm
+  vcovCRs <- function(model, types, cluster) lapply(types, function(x) vcovCR(model, type = x, cluster = cluster))
+  expect_equivalent(CR_pdata, vcovCRs(plm_numeric, CR_types, cluster = Grunfeld$firm))
+  expect_equivalent(CR_pdata, vcovCRs(plm_noindex, CR_types, cluster = Grunfeld_scramble$firm))
+  expect_equivalent(CR_pdata, vcovCRs(plm_oneindex, CR_types, cluster = Grunfeld_scramble$firm))
+  expect_equivalent(CR_pdata, vcovCRs(plm_twoindex, CR_types, cluster = Grunfeld_scramble$firm))
+
+  # manual clustering on arbitrary id
+  CR_pdata <- vcovCRs(plm_pdata, CR_types, cluster = Grunfeld_pdata$cluster)
+  expect_equivalent(CR_pdata, vcovCRs(plm_numeric, CR_types, cluster = Grunfeld$cluster))
+  expect_equivalent(CR_pdata, vcovCRs(plm_noindex, CR_types, cluster = Grunfeld_scramble$cluster))
+  expect_equivalent(CR_pdata, vcovCRs(plm_oneindex, CR_types, cluster = Grunfeld_scramble$cluster))
+  expect_equivalent(CR_pdata, vcovCRs(plm_twoindex, CR_types, cluster = Grunfeld_scramble$cluster))
+  
+})
+
