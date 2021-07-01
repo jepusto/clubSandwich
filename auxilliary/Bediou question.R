17d0ac07a96320b71d55cd54aee85cc1570e9a0e
jepusto <jepusto@gmail.com>
2021-07-01 16:56:01 -0500

    Fiddling.

--- auxilliary/Bediou question.R
+++ auxilliary/Bediou question.R
@@ -4,13 +4,13 @@ load("auxilliary/es_data.RData")
 vcov_mat <- impute_covariance_matrix(es_data$g_var, 
                                      cluster = es_data$Paper,                            
                                      r = 0.8) 
+
 mlma_model<- metafor::rma.mv(g ~ 1, # Intercept only MLMA model
                              V = vcov_mat, 
                              random = ~ 1 | Paper / ES_ID,
                              sparse = TRUE,
                              data = es_data, 
                              test="t")
-## Add RVE using clubSandwich
-library(clubSandwich)
 
-CHE_model <- clubSandwich::coef_test(mlma_model, vcov = "CR2")  
+## Add RVE using clubSandwich
+CHE_model <- coef_test(mlma_model, vcov = "CR2")  
