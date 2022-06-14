71bc03382cff6d8dc372fd53bb64b9f6f48e3f57
jepusto <jepusto@gmail.com>
2022-06-14 09:35:16 -0500

    Updated lme methods to use get_data.

--- R/lme.R
+++ R/lme.R
@@ -87,7 +87,7 @@ coef_CS.lme <- function(obj)
 #' @export
 
 model_matrix.lme <- function(obj) {
-  dat <- droplevels(getData(obj))
+  dat <- droplevels(get_data(obj))
   model.matrix(formula(obj), data = dat)
 }
 
@@ -287,87 +287,6 @@ targetVariance.lme <- function(obj, cluster = nlme::getGroups(obj, level = 1)) {
     
     # expand target_list to level of clustering
     crosswalk <- data.frame(target_grps, cluster)
-    target_list <- add_bdiag(small_mats = target_list, 
-                             big_mats = matrix_list(rep(0, length(cluster)), cluster, dim = "both"),
-                             crosswalk = crosswalk)
-  }
-  
-  return(target_list)
-}
-
-
-targetVariance_old.lme <- function(obj, cluster = nlme::getGroups(obj, level = 1)) {
-  
-  if (inherits(obj, "nlme")) stop("not implemented for \"nlme\" objects")
-  
-  all_groups <- rev(obj$groups)
-  smallest_groups <- all_groups[[1]]
-  largest_groups <- all_groups[[length(all_groups)]]
-  
-  # Get level-1 variance-covariance structure as V_list
-  
-  if (is.null(obj$modelStruct$corStruct)) {
-    if (is.null(obj$modelStruct$varStruct)) {
-      V_list <- matrix_list(rep(1, length(smallest_groups)), smallest_groups, "both")
-    } else {
-      wts <- nlme::varWeights(obj$modelStruct$varStruct)[order(do.call(order, all_groups))]
-      V_list <- matrix_list(1 / wts^2, smallest_groups, "both")
-    } 
-  } else {
-    R_list <- as.list(rep(1, nlevels(smallest_groups)))
-    names(R_list) <- levels(smallest_groups)
-    R_sublist <- nlme::corMatrix(obj$modelStruct$corStruct)
-    R_list[names(R_sublist)] <- R_sublist
-    if (is.null(obj$modelStruct$varStruct)) {
-      V_list <- R_list
-    } else {
-      sd_vec <- 1 / nlme::varWeights(obj$modelStruct$varStruct)[order(do.call(order, all_groups))]
-      sd_list <- split(sd_vec, smallest_groups)
-      V_list <- Map(function(R, s) tcrossprod(s) * R, R = R_list, s = sd_list)
-    } 
-  } 
-  
-  # Get random effects structure
-  
-  if (length(all_groups) == 1) {
-    D_mat <- as.matrix(obj$modelStruct$reStruct[[1]])
-    Z_mat <- model.matrix(obj$modelStruct$reStruc, getData(obj))
-    row.names(Z_mat) <- NULL
-    Z_list <- matrix_list(Z_mat, all_groups[[1]], "row")
-    ZDZ_list <- ZDZt(D_mat, Z_list)
-    target_list <- Map("+", ZDZ_list, V_list)
-  } else {
-    D_list <- lapply(obj$modelStruct$reStruct, as.matrix)
-    Z_mat <- model.matrix(obj$modelStruct$reStruc, getData(obj))
-    Z_names <- sapply(strsplit(colnames(Z_mat), ".", fixed=TRUE), function(x) x[1])
-    row.names(Z_mat) <- NULL
-    Z_levels <- lapply(names(all_groups), function(x) Z_mat[,x==Z_names,drop=FALSE])
-    Z_levels <- Map(matrix_list, x = Z_levels, fac = all_groups, dim = "row")
-    ZDZ_lists <- Map(ZDZt, D = D_list, Z_list = Z_levels)
-    ZDZ_lists[[1]] <- Map("+", ZDZ_lists[[1]], V_list)  
-    for (i in 2:length(all_groups)) {
-      ZDZ_lists[[i]] <- add_bdiag(small_mats = ZDZ_lists[[i-1]], 
-                                  big_mats = ZDZ_lists[[i]], 
-                                  crosswalk = all_groups[c(i-1,i)])
-    }
-    target_list <- ZDZ_lists[[i]]
-  }
-  
-  # check if clustering level is higher than highest level of random effects
-  
-  tb_groups <- table(largest_groups)
-  tb_cluster <- table(cluster)
-  if (length(tb_groups) < length(tb_cluster) | 
-      any(as.vector(tb_groups) != rep(as.vector(tb_cluster), length.out = length(tb_groups))) | 
-      any(names(tb_groups) != rep(names(tb_cluster), length.out = length(tb_groups)))) {
-    
-    # check that random effects are nested within clusters  
-    tb_cross <- table(largest_groups, cluster)
-    nested <- apply(tb_cross, 1, function(x) sum(x > 0) == 1)
-    if (!all(nested)) stop("Random effects are not nested within clustering variable.")
-    
-    # expand target_list to level of clustering
-    crosswalk <- data.frame(largest_groups, cluster)
     target_list <- add_bdiag(small_mats = target_list, 
                              big_mats = matrix_list(rep(0, length(cluster)), cluster, dim = "both"),
                              crosswalk = crosswalk)
