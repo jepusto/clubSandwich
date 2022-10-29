a47564aac35392371b9f23f9334073d965768b34
jepusto <jepusto@gmail.com>
2022-10-29 15:42:51 -0500

    Fit geeglm with corstr = "userdefined". Added model with toeplitz structure.

--- tests/testthat/test_geeglm.R
+++ tests/testthat/test_geeglm.R
@@ -38,19 +38,41 @@ geeglm_unstr <- geeglm(yvar ~ tvar, id = idvar, data = simdat, corstr = "unstruc
 geeglm_unstr
 
 # User-defined
-zcor <- genZcor(clusz = table(simdatPerm$idvar), waves = wav, corstrv = "unstructured") # nrow = 40, have attributes at the end
-geeglm_user <- geeglm(yvar ~ tvar, id = idvar, data = simdatPerm, zcor = zcor, corstr = "userdefined")
+zcor <- genZcor(clusz = table(simdat$idvar), 
+                waves = simdat$timeorder, 
+                corstrv = 4)
+nrow(zcor)
+clusz <- table(simdat$idvar)
+sum(clusz * (clusz - 1) / 2)
+
+geeglm_user <- geeglm(yvar ~ tvar, 
+                      id = idvar, waves = timeorder, 
+                      data = simdat, 
+                      zcor = zcor, corstr = "userdefined")
+
+zcor_toep     <- matrix(NA, nrow(zcor), 4)
+zcor_toep[,1] <- apply(zcor[,c(1, 5, 8,10)], 1, sum)
+zcor_toep[,2] <- apply(zcor[,c(2, 6, 9)], 1, sum)
+zcor_toep[,3] <- apply(zcor[,c(3, 7)], 1, sum)
+zcor_toep[,4] <- zcor[,4]
+
+geeglm_toep <- geeglm(yvar ~ tvar, 
+                      id = idvar, waves = timeorder, 
+                      data = simdat, 
+                      zcor = zcor_toep, corstr = "userdefined")
+
 
 
 test_that("bread works", {
   
-  expect_true(check_bread(geeglm_AR1_wav, cluster = simdatPerm$idvar, y = simdatPerm$yvar))
-  expect_true(check_bread(geeglm_AR1, cluster = simdat$idvar, y = simdat$yvar))
+  expect_true(check_bread(geeglm_AR1_wav, cluster = simdatPerm$idvar, y = simdatPerm$yvar, tol = 1e-5))
+  expect_true(check_bread(geeglm_AR1, cluster = simdat$idvar, y = simdat$yvar, tol = 1e-5))
   expect_true(check_bread(geeglm_ind, cluster = simdat$idvar, y = simdat$yvar))
   expect_true(check_bread(geeglm_exch, cluster = simdat$idvar, y = simdat$yvar))
-  expect_true(check_bread(geeglm_unstr, cluster = simdat$idvar, y = simdat$yvar)) # check bread sandwich
-  # expect_true(check_bread(geeglm_user, cluster = simdat$idvar, y = simdat$yvar))
-
+  expect_true(check_bread(geeglm_unstr, cluster = simdat$idvar, y = simdat$yvar))
+  expect_true(check_bread(geeglm_user, cluster = simdat$idvar, y = simdat$yvar))
+  expect_true(check_bread(geeglm_toep, cluster = simdat$idvar, y = simdat$yvar))
+  
 })
 
 
@@ -93,75 +115,74 @@ test_that("vcovCR options work for CR2", {
   expect_equal(vcovCR(geeglm_AR1_wav, type = "CR2", cluster = simdatPerm$idvar, 
                         target = target, inverse_var = FALSE), CR2_AR1_wav, check.attributes = FALSE)
     
-  })
+})
   
   
-  test_that("vcovCR options work for CR4", {
-    CR4_AR1_wav <- vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4")
-    expect_equal(vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4"), CR4_AR1_wav)
-    expect_equal(vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4", inverse_var = TRUE), CR4_AR1_wav)
-    expect_false(identical(vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4", inverse_var = FALSE), CR4_AR1_wav))
-    
-    target <- targetVariance(geeglm_AR1_wav)
-    expect_equal(vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4", 
-                        target = target, inverse_var = TRUE), CR4_AR1_wav, check.attributes = FALSE)
-    attr(CR4_AR1_wav, "inverse_var") <- FALSE
-    expect_equal(vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4", 
-                        target = target, inverse_var = FALSE), CR4_AR1_wav, check.attributes = FALSE)
-  })
+test_that("vcovCR options work for CR4", {
+  CR4_AR1_wav <- vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4")
+  expect_equal(vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4"), CR4_AR1_wav)
+  expect_equal(vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4", inverse_var = TRUE), CR4_AR1_wav)
+  expect_false(identical(vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4", inverse_var = FALSE), CR4_AR1_wav))
   
-  
-  test_that("CR2 and CR4 are target-unbiased", {
-    expect_true(check_CR(geeglm_AR1_wav, cluster = simdatPerm$idvar, vcov = "CR2"))
-    expect_true(check_CR(geeglm_AR1_wav, cluster = simdatPerm$idvar,vcov = "CR2"))
-    expect_true(check_CR(geeglm_AR1_wav, cluster = simdatPerm$idvar,vcov = "CR4"))
-    expect_true(check_CR(geeglm_AR1_wav, cluster = simdatPerm$idvar,vcov = "CR4"))
-  })
-  
-  test_that("get_data works.", {
-    re_order <- sample(nrow(simdatPerm))
-    sim_scramble <- simdatPerm[re_order,]
-    geeglm_AR1_wav_scramble <- geeglm(yvar ~ tvar, id = idvar, 
-                          data = sim_scramble, 
-                          corstr = "ar1", waves = wav)
-    scramble_dat <- get_data(geeglm_AR1_wav_scramble)
-    expect_equal(sim_scramble, scramble_dat)
-  })
-  
-  
-  CR_types <- paste0("CR",0:4)
-  
-  test_that("Order doesn't matter.", {
-    
-    check_sort_order(geeglm_ind, dat = simdat, cluster = simdat$idvar,
-                     tol = 10^-4, tol2 = 10^-3, tol3 = 10^-3)
-    
-  }) # Error
-  
-  
-  test_that("clubSandwich works with dropped observations", {
-    dat_miss <- simdat
-    dat_miss$yvar[sample.int(nrow(simdat), size = round(nrow(simdat) / 10))] <- NA
-    dat_dropped <- geeglm(yvar ~ tvar, id = idvar, 
-                         data = dat_miss, corstr = "independence")
-    dat_complete <- geeglm(yvar ~ tvar, id = idvar, 
-                                   data = simdat, corstr = "independence")
-    
-    CR_drop <- lapply(CR_types, function(x) vcovCR(dat_dropped, cluster = simdat$idvar, type = x))
-    CR_complete <- lapply(CR_types, function(x) vcovCR(dat_complete, cluster = simdat$idvar, type = x))
-    expect_equal(CR_drop, CR_complete)
-    
-    test_drop <- lapply(CR_types, function(x) coef_test(dat_dropped, cluster = simdat$idvar, vcov = x, test = "All", p_values = FALSE))
-    test_complete <- lapply(CR_types, function(x) coef_test(dat_complete, cluster = simdat$idvar, vcov = x, test = "All", p_values = FALSE))
-    expect_equal(test_drop, test_complete)
-  })
+  target <- targetVariance(geeglm_AR1_wav)
+  expect_equal(vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4", 
+                      target = target, inverse_var = TRUE), CR4_AR1_wav, check.attributes = FALSE)
+  attr(CR4_AR1_wav, "inverse_var") <- FALSE
+  expect_equal(vcovCR(geeglm_AR1_wav, cluster = simdatPerm$idvar, type = "CR4", 
+                      target = target, inverse_var = FALSE), CR4_AR1_wav, check.attributes = FALSE)
+})
 
+
+test_that("CR2 and CR4 are target-unbiased", {
+  expect_true(check_CR(geeglm_AR1_wav, cluster = simdatPerm$idvar, vcov = "CR2"))
+  expect_true(check_CR(geeglm_AR1_wav, cluster = simdatPerm$idvar,vcov = "CR2"))
+  expect_true(check_CR(geeglm_AR1_wav, cluster = simdatPerm$idvar,vcov = "CR4"))
+  expect_true(check_CR(geeglm_AR1_wav, cluster = simdatPerm$idvar,vcov = "CR4"))
+})
+
+test_that("get_data works.", {
+  re_order <- sample(nrow(simdatPerm))
+  sim_scramble <- simdatPerm[re_order,]
+  geeglm_AR1_wav_scramble <- geeglm(yvar ~ tvar, id = idvar, 
+                        data = sim_scramble, 
+                        corstr = "ar1", waves = wav)
+  scramble_dat <- get_data(geeglm_AR1_wav_scramble)
+  expect_equal(sim_scramble, scramble_dat)
+})
+
+
+CR_types <- paste0("CR",0:4)
+
+test_that("Order doesn't matter.", {
   
-  # add check.attributes = F to expect_equal
-  # vcovCR for geeglm is using bread function from sandwich package
-  # userdefined  genZcor function generate output with attributes at the end, nrow is not correct
-  # "bread works": not for the unstructured
-  # "vcovCR works for clustering variables higher than id variable." & "Order doesn't matter." has problem.
-  # "clubSandwich works with dropped observations"
+  check_sort_order(geeglm_ind, dat = simdat, cluster = simdat$idvar,
+                   tol = 10^-4, tol2 = 10^-3, tol3 = 10^-3)
+  
+}) # Error
   
   
+test_that("clubSandwich works with dropped observations", {
+  dat_miss <- simdat
+  dat_miss$yvar[sample.int(nrow(simdat), size = round(nrow(simdat) / 10))] <- NA
+  dat_dropped <- geeglm(yvar ~ tvar, id = idvar, 
+                       data = dat_miss, corstr = "independence")
+  dat_complete <- geeglm(yvar ~ tvar, id = idvar, 
+                                 data = simdat, corstr = "independence")
+  
+  CR_drop <- lapply(CR_types, function(x) vcovCR(dat_dropped, cluster = simdat$idvar, type = x))
+  CR_complete <- lapply(CR_types, function(x) vcovCR(dat_complete, cluster = simdat$idvar, type = x))
+  expect_equal(CR_drop, CR_complete)
+  
+  test_drop <- lapply(CR_types, function(x) coef_test(dat_dropped, cluster = simdat$idvar, vcov = x, test = "All", p_values = FALSE))
+  test_complete <- lapply(CR_types, function(x) coef_test(dat_complete, cluster = simdat$idvar, vcov = x, test = "All", p_values = FALSE))
+  expect_equal(test_drop, test_complete)
+})
+
+
+# add check.attributes = F to expect_equal
+# vcovCR for geeglm is using bread function from sandwich package
+# userdefined  genZcor function generate output with attributes at the end, nrow is not correct
+# "bread works": not for the unstructured
+# "vcovCR works for clustering variables higher than id variable." & "Order doesn't matter." has problem.
+# "clubSandwich works with dropped observations"
+
