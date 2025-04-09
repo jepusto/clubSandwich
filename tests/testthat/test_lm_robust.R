f8e70074cd1ccaef22df2341df10e4e83c48e584
Samuel Pekofsky <sampekofsky@gmail.com>
2025-04-09 13:00:09 -0500

    Most updated versions of lm_robust.R and test_lm_robust.R for Zoom meeting.

--- tests/testthat/test_lm_robust.R
+++ tests/testthat/test_lm_robust.R
@@ -15,72 +15,72 @@ lm_rob <- lm_robust(weight ~ 0 + Diet + Time:Diet, data = ChickWeight)
 
 # needs model_matrix to work???
 test_that("vcovCR works", {
-  vcov_lm <- vcovCR(lm_fit, ChickWeight$Chick, "CR2") # works
+  vcov_lm <- vcovCR(lm_fit, ChickWeight$Chick, "CR2")
   vcov_lmr <- vcovCR(lm_rob, ChickWeight$Chick, "CR2") # doesn't work
   
-  expect_equal(vcov_lm, vcov_lmr)
+  expect_equal(vcov_lm, vcov_lmr) # doesn't work
 })
 
 # =============== model_matrix() ===============
 
 test_that("model_matrix() works", {
-  mm_fit <- model_matrix(lm_fit) # works
-  mm_rob <- model_matrix(lm_rob) # doesn't work
+  mm_fit <- model_matrix(lm_fit) 
+  mm_rob <- model_matrix(lm_rob) # works as of recently
   
-  expect_equal(mm_fit, mm_rob)
+  expect_equal(mm_fit, mm_rob) # works as of recently
 })
 
 # =============== residuals_CS() ===============
 
 test_that("residuals_CS() works", {
-  rcs_fit <- residuals_CS(lm_fit) # works
-  rcs_rob <- residuals_CS(lm_rob) # gives null
+  rcs_fit <- residuals_CS(lm_fit)
+  rcs_rob <- residuals_CS(lm_rob, data = ChickWeight)
   
-  expect_equal(rcs_fit, rcs_rob)
+  expect_equal(rcs_fit, rcs_rob) # doesn't work
 })
 
 # =============== coef() ===============
 
 test_that("coef() works", {
-  coef_fit <- coef(lm_fit) # works
-  coef_rob <- coef(lm_rob) # works?
+  coef_fit <- coef(lm_fit)
+  coef_rob <- coef(lm_rob)
   
-  expect_equal(coef_fit, coef_rob) # works!!
+  expect_equal(coef_fit, coef_rob)
 })
 
 # =============== nobs() ===============
 
 test_that("nobs() works", {
-  nobs_fit <- nobs(lm_fit) # works
-  nobs_rob <- nobs(lm_rob) # works
+  nobs_fit <- nobs(lm_fit)
+  nobs_rob <- nobs(lm_rob)
   
-  expect_equal(nobs_fit, nobs_rob) # works!!!
+  expect_equal(nobs_fit, nobs_rob)
 })
 
 
 # =============== targetVariance() ===============
 
 test_that("targetVariance() works", {
-  tV_fit <- targetVariance(lm_fit, ChickWeight$Chick) # works
-  tV_rob <- targetVariance(lm_rob, ChickWeight$Chick) # also works
+  tV_fit <- targetVariance(lm_fit, ChickWeight$Chick)
+  tV_rob <- targetVariance(lm_rob, ChickWeight$Chick)
   
-  expect_equal(tV_fit, tV_rob) # works!
+  expect_equal(tV_fit, tV_rob)
 })
 
 # =============== weightMatrix() ===============
 
 test_that("weightMatrix() works", {
-  wM_fit <- weightMatrix(lm_fit, ChickWeight$Chick) # works!!
-  wM_rob <- weightMatrix(lm_rob, ChickWeight$Chick) # works!
+  wM_fit <- weightMatrix(lm_fit, ChickWeight$Chick)
+  wM_rob <- weightMatrix(lm_rob, ChickWeight$Chick)
   
-  expect_equal(wM_fit, wM_rob) # works!!!!
+  expect_equal(wM_fit, wM_rob)
 })
 
 # =============== v_scale() ===============
 
 test_that("v_scale() works", {
-  vs_fit <- v_scale(lm_fit) # works
-  vs_rob <- v_scale(lm_rob) # works
+  vs_fit <- v_scale(lm_fit)
+  vs_rob <- v_scale(lm_rob)
   
-  expect_equal(vs_fit, vs_rob) # works!!
+  expect_equal(vs_fit, vs_rob)
 })
