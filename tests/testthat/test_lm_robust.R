dc1c9140aa9b38cc8df5a462495c4e3af4ed5f1c
Samuel Pekofsky <sampekofsky@gmail.com>
2025-04-16 19:48:17 -0500

    Added tests to ensure that clusters is still pulled correctly when it is either a function such as factor(...) or a variable.

--- tests/testthat/test_lm_robust.R
+++ tests/testthat/test_lm_robust.R
@@ -288,6 +288,9 @@ test_that("vcovCR works", {
 
 test_that("vovCR properly pulls cluster specified for lm_robust", {
   
+  # reset ChickWeight
+  data("ChickWeight")
+  
   # unweighted tests
   
   uw_clust <- vcovCR(lm_rob, ChickWeight$Chick, "CR2")
@@ -297,6 +300,14 @@ test_that("vovCR properly pulls cluster specified for lm_robust", {
   expect_equal(uw_clust, uw_no_clust)
   expect_equal(uw_no_clust, uw_lm)
   
+  # create an lm_robust that draws in data differently
+  lm_rob_fact <- lm_robust(weight ~ 0 + Diet + Time:Diet, data = ChickWeight, 
+                           clusters = factor(ChickWeight$Chick, ordered = FALSE))
+  # perform vcovCR
+  uw_fact_cr <- vcovCR(lm_rob_fact, type = "CR2")
+  
+  expect_equal(uw_clust, uw_fact_cr)
+  
   # weighted tests
   
   w_clust <- vcovCR(wlm_rob, ChickWeight$Chick, "CR2")
@@ -305,5 +316,14 @@ test_that("vovCR properly pulls cluster specified for lm_robust", {
   
   expect_equal(w_clust, w_no_clust)
   expect_equal(w_no_clust, w_lm)
+  
+  # create an lm_robust that draws in data differently
+  lm_rob_fact_w <- lm_robust(weight ~ 0 + Diet + Time:Diet, weights = wt, 
+                             data = ChickWeight, 
+                             clusters = factor(ChickWeight$Chick, ordered = FALSE))
+  # perform vcovCR
+  w_fact_cr <- vcovCR(lm_rob_fact_w, type = "CR2")
+  
+  expect_equal(uw_clust, w_fact_cr)
   
 })
