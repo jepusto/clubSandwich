3485502ddebbbfa263dec5ea6c51c6d8a78a5971
Samuel Pekofsky <sampekofsky@gmail.com>
2025-04-16 19:07:44 -0500

    Improved cluster pulling test from last commit.

--- tests/testthat/test_lm_robust.R
+++ tests/testthat/test_lm_robust.R
@@ -292,14 +292,18 @@ test_that("vovCR properly pulls cluster specified for lm_robust", {
   
   uw_clust <- vcovCR(lm_rob, ChickWeight$Chick, "CR2")
   uw_no_clust <- vcovCR(lm_rob, type = "CR2")
+  uw_lm <- vcovCR(lm_fit, ChickWeight$Chick, "CR2")
   
   expect_equal(uw_clust, uw_no_clust)
+  expect_equal(uw_no_clust, uw_lm)
   
   # weighted tests
   
   w_clust <- vcovCR(wlm_rob, ChickWeight$Chick, "CR2")
   w_no_clust <- vcovCR(wlm_rob, type = "CR2")
+  w_lm <- vcovCR(lm_fit, ChickWeight$Chick, "CR2")
   
   expect_equal(w_clust, w_no_clust)
+  expect_equal(w_no_clust, w_lm)
   
 })
