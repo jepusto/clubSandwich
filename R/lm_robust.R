687efbb68254a5ccc583e626d11fa2b4ecac5afe
Samuel Pekofsky <sampekofsky@gmail.com>
2025-04-16 19:18:16 -0500

    Updated vcovCR.lm_robust and its documentation, model.frame.lm_robust (updated during 4/14/25 meeting, but not committed that day), and tidying up of model_matrix.lm_robust.

--- R/lm_robust.R
+++ R/lm_robust.R
@@ -1,37 +1,105 @@
+#-------------------------------------
+# vcovCR with defaults
+#-------------------------------------
 
-
+#' Cluster-robust variance-covariance matrix for an estimatr::lm_robust object.
+#' 
+#' \code{vcovCR} returns a sandwich estimate of the variance-covariance matrix 
+#' of a set of regression coefficient estimates from an 
+#' \code{\link{estimatr::lm_robust}} object.
+#' 
+#' @param cluster Expression or vector indicating which observations belong to
+#'   the same cluster. Required for \code{estimatr::lm_robust} objects.
+#' @param target Optional matrix or vector describing the working
+#'   variance-covariance model used to calculate the \code{CR2} and \code{CR4}
+#'   adjustment matrices. If a vector, the target matrix is assumed to be
+#'   diagonal. If not specified, the target is taken to be an identity matrix.
+#' @inheritParams vcovCR
+#'   
+#' @return An object of class \code{c("vcovCR","clubSandwich")}, which consists
+#'   of a matrix of the estimated variance of and covariances between the
+#'   regression coefficient estimates.
+#'   
+#' @seealso \code{\link{vcovCR}}
+#' 
+#' @examples 
+#' 
+#' NOTE: These are (currently) the same as the examples from vcovCR.lm
+#' 
+#' data("ChickWeight", package = "datasets")
+#' lm_fit <- lm(weight ~ Time + Diet:Time, data = ChickWeight)
+#' vcovCR(lm_fit, cluster = ChickWeight$Chick, type = "CR2")
+#' 
+#' if (requireNamespace("plm", quietly = TRUE)) withAutoprint({
+#' 
+#'   data("Produc", package = "plm")
+#'   lm_individual <- lm(log(gsp) ~ 0 + state + log(pcap) + log(pc) + log(emp) + unemp, data = Produc)
+#'   individual_index <- !grepl("state", names(coef(lm_individual)))
+#'   vcovCR(lm_individual, cluster = Produc$state, type = "CR2")[individual_index,individual_index]
+#' 
+#'   # compare to plm()
+#'   plm_FE <- plm::plm(log(gsp) ~ log(pcap) + log(pc) + log(emp) + unemp, 
+#'                      data = Produc, index = c("state","year"), 
+#'                      effect = "individual", model = "within")
+#'   vcovCR(plm_FE, type="CR2")
+#'   
+#' })
+#' 
 #' @export
-# vcovCR.lm_robust <- function(obj, cluster, type, target = NULL, inverse_var = NULL, form = "sandwich", ...) {
+vcovCR.lm_robust <- function(obj, cluster, type, target = NULL, inverse_var = NULL, form = "sandwich", ...) {
+  if (missing(cluster)) {
+    if (obj$clustered) cluster <- get_cluster(obj) # pull clustering var from obj
+    else stop("You must specify a clustering variable or your object must have one.")
+  }
+  if (is.null(inverse_var)) inverse_var <- is.null(weights(obj)) & is.null(target)
+  vcov_CR(obj, cluster = cluster, type = type, 
+          target = target, inverse_var = inverse_var, form = form)
+}
+
+
+#' Helper function written by GPT, edited by Sam
+get_cluster <- function(obj) {
+  # 1. Grab the unevaluated clusters argument
+  cluster_expr <- obj$call$clusters
   
-# }
+  # 2. Use the formula/environment of the fit as our evaluation backbone
+  fit_env <- environment(obj$terms)
+  
+  # 3. If the user passed a data= argument, pull that data in…
+  if (!is.null(obj$call$data)) {
+    data_val <- eval(obj$call$data, envir = fit_env)
+    # …and evaluate the clusters expression inside that data
+    cluster <- eval(cluster_expr, envir = data_val, enclos = fit_env)
+  } else {
+    # otherwise just eval it in the fit’s environment
+    cluster <- eval(cluster_expr, envir = fit_env)
+  }
+  
+  # 4. Done
+  cluster
+}
 
 
-# model_matrix.lm_robust <- function(obj) {
-  # model_matrix <- model.matrix(obj)
-  #
-  # w <- obj$weights
-  # if (is.null(w) || all(pos_wts <- w > 0)) {
-  #   return(model_matrix)
-  # } else {
-  #   return(model_matrix[pos_wts > 0,,drop=FALSE])
-# }
+#' Same as model.matrix.lm
 #' @export
-model.matrix.lm_robust <- function (object, ...) {
-  if (n_match <- match("x", names(object), 0L))
+model.matrix.lm_robust <- function (object, ...) 
+{
+  if (n_match <- match("x", names(object), 0L)) 
     object[[n_match]]
   else {
-    data <- model.frame(object, ...)
-    if (exists(".GenericCallEnv", inherits = FALSE))
+    data <- model.frame(object, xlev = object$xlevels, ...)
+    if (exists(".GenericCallEnv", inherits = FALSE)) 
       NextMethod("model.matrix", data = data, contrasts.arg = object$contrasts)
     else {
       dots <- list(...)
       dots$data <- dots$contrasts.arg <- NULL
-      do.call("model.matrix.default", c(list(object = object,
-                                             data = data, contrasts.arg = object$contrasts),
-                                        dots))
+      do.call("model.matrix.default", c(list(object = object, 
+        data = data, contrasts.arg = object$contrasts), 
+        dots))
     }
   }
 }
+
 
 # written by GPT, slightly edited by Sam
 #' @export
@@ -65,14 +133,12 @@ bread.lm_robust <- function(obj, ...) {
   
   X <- model_matrix(obj)
   
-  w <- model.frame(obj)$weight
-  
-  XtWX <- crossprod(X, w * X)
-  
-  # p <- obj$rank
-  # p1 <- 1L:p
-  # R <- chol2inv(obj$qr[p1, p1, drop = FALSE])
-  # return(N * R)
+  if(obj$weighted) {
+    XtWX <- crossprod(X, obj$weights * X)
+  }
+  else {
+    XtWX <- crossprod(X)
+  }
   
   return(N * solve(XtWX))
 }
